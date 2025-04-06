0fd4a92ac92db65d386f894d286a15ae7a4b6837
Yeongeunn <115203177+Yeongeunn@users.noreply.github.com>
2025-04-06 23:50:57 +0900

    feat: 루틴 페이지 UI 개선 및 버튼 탭 전환 기능 구현 (#5)

--- myproject/settings.py
+++ myproject/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '0718',
+        'PASSWORD': 'sksk0877',
         'HOST': 'localhost',
         'PORT': '5432',
     }
